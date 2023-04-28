32cab4fda913dfcfba790edfff86aef35ed229fe
RicardoUy <rvuy62@yahoo.com>
2023-04-28 16:00:07 -0500

    submit superbowl

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "L"},
+    { year: "2014", result: "W"},
+    { year: "2013", result: "L"}
+    
+  ]
+
+function superbowlWin(record){
+    let winner = record.find(({result}) => result === "W")
+  if (winner) {
+    return winner["year"]
+  }
+}
+console.log(superbowlWin(record))
